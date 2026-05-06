fbbe107ae784df13209f0a1a62c85867381f9b2c
keziazahiljohn <keziazahiljohn@gmail.com>
2026-05-06 13:15:52 -0400

    Name formatter

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
 public class NameFormatter {
+    public static String format (String firstName,
+                                 String lastName) {
+        return lastName + ", " + firstName;
+    }
+
+    public static String format (String prefix,
+                                 String firstName,
+                                 String middleName,
+                                 String lastName,
+                                 String suffix) {
+        return lastName + ", " + prefix + " " + firstName + " " + middleName + ", " + suffix;
+    }
+
+    public static String format (String fullName) {
+        return fullName;
+    }
 }
